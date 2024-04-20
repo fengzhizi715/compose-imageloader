74d115bd79785118490b8dcac63f7b876f870419
Tony Shen <fengzhizi715@126.com>
2024-04-20 22:04:19 +0800

    update

--- desktop/src/jvmMain/kotlin/App.kt
+++ desktop/src/jvmMain/kotlin/App.kt
@@ -12,20 +12,7 @@ import cn.netdiscovery.compose.imageloader.core.ImageLoaderFactory.USER_DIR
 import cn.netdiscovery.compose.imageloader.core.ImageCallback
 import cn.netdiscovery.compose.imageloader.imageUrl
 import cn.netdiscovery.compose.imageloader.transform.Transformation
-
-val imageList = arrayListOf(
-    "https://w.wallhaven.cc/full/wq/wallhaven-wq2787.jpg",
-    "https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png",
-    "https://w.wallhaven.cc/full/6o/wallhaven-6ozkzl.jpg",
-    "https://w.wallhaven.cc/full/z8/wallhaven-z8dg9y.png",
-    "https://w.wallhaven.cc/full/j3/wallhaven-j3m8y5.png",
-    "https://w.wallhaven.cc/full/y8/wallhaven-y8622k.jpg",
-    "https://w.wallhaven.cc/full/57/wallhaven-572k81.png",
-    "https://w.wallhaven.cc/full/72/wallhaven-72ywpv.jpg",
-    "https://w.wallhaven.cc/full/v9/wallhaven-v9wo18.png",
-    "https://w.wallhaven.cc/full/8o/wallhaven-8o23gk.jpg",
-    "https://w.wallhaven.cc/full/pk/wallhaven-pkq1q9.png"
-)
+import kotlin.random.Random
 
 @Composable
 fun defaultPlaceHolderView() {
@@ -36,8 +23,7 @@ fun defaultPlaceHolderView() {
 
 @Composable
 fun App() {
-    var imageUrl by remember { mutableStateOf(imageList.first()) }
-    var lastIndex by remember { mutableStateOf(0) }
+
     var transformation by remember { mutableStateOf(Transformation().toList()) }
 
     LaunchedEffect(Unit) {
@@ -50,26 +36,7 @@ fun App() {
             modifier = Modifier.fillMaxWidth().background(Color.White).padding(32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            Row(
-//                modifier = Modifier.fillMaxWidth(),
-//                verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.Center
-//            ) {
-//                ImageRes(
-//                    "compose-logo.png",
-//                    imageCallback = ImageCallback.default(modifier = Modifier.size(64.dp), contentDescription = "res")
-//                )
-//                ImageAsyncSvgUrl("https://raw.githubusercontent.com/JetBrains/compose-jb/master/artwork/compose-logo.svg",
-//                    density = LocalDensity.current,
-//                    imageCallback = ImageCallback {
-//                        Image(modifier = Modifier.size(64.dp), painter = it, contentDescription = "svg")
-//                    })
-//                ImageAsyncVectorUrl("https://raw.githubusercontent.com/JetBrains/compose-jb/master/artwork/compose-logo.xml",
-//                    density = LocalDensity.current,
-//                    imageCallback = ImageCallback {
-//                        Image(modifier = Modifier.size(64.dp), painter = it, contentDescription = "vector")
-//                    })
-//            }
+
             Spacer(modifier = Modifier.height(16.dp))
             Box(modifier = Modifier.weight(1f).fillMaxWidth(), contentAlignment = Alignment.Center) {
                 imageUrl(imageUrl,
@@ -80,45 +47,6 @@ fun App() {
                         Image(modifier = Modifier.size(800.dp), painter = it, contentDescription = "123")
                     })
             }
-//            Spacer(modifier = Modifier.height(16.dp))
-//            Button(modifier = Modifier, onClick = {
-//                var random = Random.nextInt(0, imageList.size)
-//                while (lastIndex == random) {
-//                    random = Random.nextInt(0, imageList.size)
-//                }
-//                lastIndex = random
-//                imageUrl = imageList[random]
-//            }) {
-//                Text(text = "Change")
-//            }
-//            Spacer(modifier = Modifier.height(16.dp))
-//            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-//                Button(modifier = Modifier, onClick = {
-//                    transformation = Transformation().circleCrop(
-//                        strokeWidth = 60f, strokeColor = Color.Red, backgroundColor = Color.Gray
-//                    ).toList()
-//                }) {
-//                    Text(text = "Circle")
-//                }
-//                Spacer(modifier = Modifier.width(16.dp))
-//                Button(modifier = Modifier, onClick = {
-//                    transformation = Transformation().none().toList()
-//                }) {
-//                    Text(text = "Normal")
-//                }
-//                Spacer(modifier = Modifier.width(16.dp))
-//                Button(modifier = Modifier, onClick = {
-//                    transformation = Transformation().resize(120, 120).toList()
-//                }) {
-//                    Text(text = "Resize 120x120")
-//                }
-//                Spacer(modifier = Modifier.width(16.dp))
-//                Button(modifier = Modifier, onClick = {
-//                    transformation = Transformation().centerCrop(240, 240).toList()
-//                }) {
-//                    Text(text = "CenterCrop 240x240")
-//                }
-//            }
         }
     }
 }
--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,17 +1,58 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlin.random.Random
 
 
 const val previewWidth = 600
 
+val imageList = arrayListOf(
+    "https://w.wallhaven.cc/full/wq/wallhaven-wq2787.jpg",
+    "https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png",
+    "https://w.wallhaven.cc/full/6o/wallhaven-6ozkzl.jpg",
+    "https://w.wallhaven.cc/full/z8/wallhaven-z8dg9y.png",
+    "https://w.wallhaven.cc/full/j3/wallhaven-j3m8y5.png",
+    "https://w.wallhaven.cc/full/y8/wallhaven-y8622k.jpg",
+    "https://w.wallhaven.cc/full/57/wallhaven-572k81.png",
+    "https://w.wallhaven.cc/full/72/wallhaven-72ywpv.jpg",
+    "https://w.wallhaven.cc/full/v9/wallhaven-v9wo18.png",
+    "https://w.wallhaven.cc/full/8o/wallhaven-8o23gk.jpg",
+    "https://w.wallhaven.cc/full/pk/wallhaven-pkq1q9.png"
+)
+
+var imageUrl by mutableStateOf(imageList.first())
+
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
+
     Window(
         title = "ImageLoader 演示程序",
         state = rememberWindowState(width = Dp(previewWidth * 2.toFloat()), height = 900.dp),
         onCloseRequest = ::exitApplication) {
+        MenuBar{
+            Menu(text = "Demo", mnemonic = 'D') {
+                Item(
+                    text = "随机显示图片",
+                    onClick = {
+                        var random = Random.nextInt(0, imageList.size-1)
+                        imageUrl = imageList[random]
+                    },
+                    //显示快捷键
+                    shortcut = KeyShortcut(Key.C, ctrl = true)
+                )
+            }
+        }
+
         App()
     }
 }
