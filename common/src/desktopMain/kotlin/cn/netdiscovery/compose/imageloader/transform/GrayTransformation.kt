52c00d7ed1595d38ae7dd79938936cbb53b00ca2
Tony Shen <fengzhizi715@126.com>
2024-04-23 10:17:59 +0800

    Create GrayTransformation.kt

---
+++ common/src/desktopMain/kotlin/cn/netdiscovery/compose/imageloader/transform/GrayTransformation.kt
@@ -0,0 +1,39 @@
+package cn.netdiscovery.compose.imageloader.transform
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toAwtImage
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.compose.imageloader.transform.GrayTransformation
+ * @author: Tony Shen
+ * @date: 2024/4/22 19:53
+ * @version: V1.0  Gray = R*0.299+G*0.587+B*0.114
+ */
+class GrayTransformation: Transformer {
+
+    override fun transform(imageBitmap: ImageBitmap): ImageBitmap {
+        val width: Int = imageBitmap.width
+        val height: Int = imageBitmap.height
+
+        val image:BufferedImage = imageBitmap.toAwtImage()
+
+        for (i in 0 until width) {
+            for (j in 0 until height) {
+                val rgb = image.getRGB(i, j)
+                val r = rgb and (0x00ff0000 shr 16)
+                val g = rgb and (0x0000ff00 shr 8)
+                val b = rgb and 0x000000ff
+
+                val color = (r * 0.299 + g * 0.587 + b * 0.114).toInt()
+                image.setRGB(i, j, Color(color, color, color).rgb)
+            }
+        }
+
+        return image.toComposeImageBitmap()
+    }
+}
